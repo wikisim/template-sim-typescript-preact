8aba72d23ea1e0a9ebeb664e87fe64b8bc2bb752
Alexander James Phillips <ajp.global@gmail.com>
2025-06-20 15:51:08 +0100

    Initial commit

---
+++ src/DemoSim.tsx
@@ -0,0 +1,11 @@
+import logo from "./assets/logo.svg"
+
+
+export const DemoSim = () => {
+    return <div>
+        <img src={logo} alt="Logo" style={{ width: 64, height: 64 }} />
+        <h3>Demo Simulation</h3>
+        <p>This is a placeholder for the demo simulation component.</p>
+        {/* Add your simulation content here */}
+    </div>
+}
