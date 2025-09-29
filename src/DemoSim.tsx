47283bf86d7fd1a6c00b5422b4a95ad9469007d2
Alexander James Phillips <ajp.global@gmail.com>
2025-09-29 20:58:13 +0100

    Simplify loading and setting up demo simulation

--- src/DemoSim.tsx
+++ src/DemoSim.tsx
@@ -3,23 +3,11 @@ import { useEffect, useState } from "preact/hooks"
 import { request_historical_data_components, RequestDataComponentsReturn } from "core/data/fetch_from_db"
 import { format_data_component_value_to_string } from "core/data/format/format_data_component_value_to_string"
 import { IdAndVersion } from "core/data/id"
+import { DataComponent } from "core/data/interface"
 import { get_supabase } from "core/supabase/browser"
 
-import logo from "./assets/logo.svg"
 
-
-export const DemoSim = () => {
-    return <div>
-        <img src={logo} alt="Logo" style={{ width: 64, height: 64 }} />
-        <h3>Demo Simulation</h3>
-        <p>This is a placeholder for the demo simulation component.</p>
-        <LoadData />
-        {/* Add your simulation content here */}
-    </div>
-}
-
-
-function LoadData()
+export const DemoSim = () =>
 {
     const [response, set_response] = useState<RequestDataComponentsReturn | null>(null)
 
@@ -32,19 +20,25 @@ function LoadData()
     return <div>
         {response === null && <p>Loading data from WikiSim...</p>}
         {response && response.error && <p>Error loading data from WikiSim: {response.error.message}</p>}
-        {response && response.data && <>
-            Loaded {response.data.length} data components from WikiSim:
-            <ul>
-                {response.data.map((component, index) => (
-                    <div key={component.id.to_str()}>
-                        <strong>ID:</strong> {component.id.id}<br/>
-                        <strong>Version:</strong> {component.id.version}<br/>
-                        <strong>Title:</strong> {component.title}<br/>
-                        <strong>Value:</strong> {component.result_value}<br/>
-                        <strong>Value as text:</strong> {format_data_component_value_to_string(component)}<br/>
-                    </div>
-                ))}
-            </ul>
-        </>}
+        {response && response.data && <Simulation data={response.data} />}
+    </div>
+}
+
+
+function Simulation(props: { data: DataComponent[] })
+{
+    return <div>
+        Loaded {props.data.length} data components from WikiSim:
+        <ul>
+            {props.data.map(component => (
+                <div key={component.id.to_str()}>
+                    <strong>ID:</strong> {component.id.id}<br/>
+                    <strong>Version:</strong> {component.id.version}<br/>
+                    <strong>Title:</strong> {component.title}<br/>
+                    <strong>Value:</strong> {component.result_value}<br/>
+                    <strong>Value as text:</strong> {format_data_component_value_to_string(component)}<br/>
+                </div>
+            ))}
+        </ul>
     </div>
 }
