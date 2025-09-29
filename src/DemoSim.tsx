696c3ff30dbfd1f50c75f8b9866c9cfff94d8826
Alexander James Phillips <ajp.global@gmail.com>
2025-09-29 20:53:30 +0100

    Fix loading demo data

--- src/DemoSim.tsx
+++ src/DemoSim.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "preact/hooks"
 
-import { request_archived_data_components, RequestDataComponentsReturn } from "core/data/fetch_from_db"
+import { request_historical_data_components, RequestDataComponentsReturn } from "core/data/fetch_from_db"
 import { format_data_component_value_to_string } from "core/data/format/format_data_component_value_to_string"
 import { IdAndVersion } from "core/data/id"
-import { get_supabase } from "core/supabase"
+import { get_supabase } from "core/supabase/browser"
 
 import logo from "./assets/logo.svg"
 
@@ -25,7 +25,7 @@ function LoadData()
 
     useEffect(() =>
     {
-        request_archived_data_components(get_supabase, [new IdAndVersion(10, 1)])
+        request_historical_data_components(get_supabase, [new IdAndVersion(1002, 6)])
         .then(set_response)
     }, [])
 
@@ -36,13 +36,13 @@ function LoadData()
             Loaded {response.data.length} data components from WikiSim:
             <ul>
                 {response.data.map((component, index) => (
-                    <li key={index}>
-                        <strong>ID:</strong> {component.id.id},
-                        <strong>Version:</strong> {component.id.version},
-                        <strong>Title:</strong> {component.title},
-                        <strong>Value:</strong> {component.value},
-                        <strong>Value as text:</strong> {format_data_component_value_to_string(component)}
-                    </li>
+                    <div key={component.id.to_str()}>
+                        <strong>ID:</strong> {component.id.id}<br/>
+                        <strong>Version:</strong> {component.id.version}<br/>
+                        <strong>Title:</strong> {component.title}<br/>
+                        <strong>Value:</strong> {component.result_value}<br/>
+                        <strong>Value as text:</strong> {format_data_component_value_to_string(component)}<br/>
+                    </div>
                 ))}
             </ul>
         </>}
