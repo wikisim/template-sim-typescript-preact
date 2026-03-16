79e3445c0d7a4b5028082b474024c409bd403002
Alexander James Phillips <ajp.global@gmail.com>
2026-03-16 13:37:14 +0000

    Add vite plugins to make it easier to work with static imports. Fixes #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import preact from "@preact/preset-vite"
 import { resolve } from "path"
 import { defineConfig } from "vite"
+// import restart from "vite-plugin-restart"
+
+import {
+    check_asset_imports_plugin,
+    copy_assets_files_plugin,
+} from "./vite_handle_redirected_files"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,10 +25,24 @@ export default defineConfig({
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
-    plugins: [preact()],
+    plugins: [
+        preact(),
+        // restart({ restart: [ "../public/**", ] }), // Restart server on file changes to public/
+        copy_assets_files_plugin(asset_white_list),
+        check_asset_imports_plugin(),
+    ],
     resolve: {
         alias: {
             "core": resolve(__dirname, "./lib/core/src"),
         }
     },
 })
+
+
+function asset_white_list(path: string, entry: string): boolean
+{
+    // if (entry.endsWith("_boundary.geojson")) return true
+    // if (path.match(/.*src\/assets\/scale_.*\.png/)) return true
+
+    return false
+}
